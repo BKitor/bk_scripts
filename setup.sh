f7d6d2b30600fe0c7728451bf845da9ee8a69568
bkitor <bkitor@gmail.com>
2022-01-25 23:09:09 -0500

    setup script

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+
+
+if [ -e "$HOME/.profile" ]; then 
+	OUT_FILE="$HOME/.profile"
+elif [ -e "$HOME/.bash_profile" ]; then
+	OUT_FILE="$HOME/.bash_profile"
+else
+	OUT_FILE="$HOME/.bashrc"
+fi	
+
+echo "PATH=\"$PWD/bin:\$PATH" >> $OUT_FILE
